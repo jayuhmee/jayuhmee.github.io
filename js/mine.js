f281420741ea6ced1aea528e75d11da3538d6d82
Jaime S <j.me@cloudflare.com>
2014-12-03 21:43:58 -0800

    moar!

--- js/mine.js
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript">
-    $(document).ready(function () {
-        // example
-        $("#example").gridalicious({
-            width: 100
-        });
-    });
-</script>
-
